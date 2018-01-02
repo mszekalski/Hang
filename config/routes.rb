dff742aaa19106ce7cb6f7d07049579bdacb6f29
Matthew Szekalski <mattszekalski@icloud.com>
2018-01-02 14:37:56 -0500

    added webpack config and set up frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
-    resource :session only [:create, :destroy, :show]
-    resources :users only [:create]
+    resource :session, only: [:create, :destroy, :show]
+    resources :user, only: [:create]
   end
 
+  root "static_pages#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
