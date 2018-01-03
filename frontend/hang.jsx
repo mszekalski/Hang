92e7a90eb5d6c7b6fe5ecfb510a83a913abdef88
Matthew Szekalski <mattszekalski@icloud.com>
2018-01-03 09:41:38 -0500

    finished reducers and added store to DOM

--- frontend/hang.jsx
+++ frontend/hang.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store.js';
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
   ReactDOM.render(<h1>Does it work?</h1>, root);
 });
