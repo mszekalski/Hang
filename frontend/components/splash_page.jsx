8998e823e0ff503844a09444c9e3b3b2ada9b7c3
Matthew Szekalski <mattszekalski@icloud.com>
2018-01-05 12:39:16 -0500

    styled splash page

--- frontend/components/splash_page.jsx
+++ frontend/components/splash_page.jsx
@@ -1,14 +1,29 @@
 import React from "react";
+import { Route, Link } from 'react-router-dom';
 
 export default function() {
   return (
-    <div>
-
+    <div className="splash-page-container">
       <div className="image-container">
-
+        <img className="splash-image" src={window.staticImages.splashImage}/>
       </div>
-      <div className="content-container">
-        <div className="splash-slogan">Where Productivity Occurs</div>
+      <div className="splash-content-container">
+        <h1 className="splash-slogan">Where Productivity Occurs</h1>
+        <p className="splash-paragraph">When your team needs to kick off a project,
+          hire a new employee, deploy some code,
+          review a sales contract,
+          finalize next year's budget,
+          measure an A/B test, plan your next office opening, and more,
+          Hang has you covered.</p>
+
+          <span>
+              <Link to="/signup">Get Started</Link>
+          </span>
+
+          <span>Already using Hang?
+              <Link to="/login"> Sign in.</Link>
+          </span>
+
       </div>
     </div>
   );
