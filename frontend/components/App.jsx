79b818dddb072e1997ec06784163720f27ab7c91
Matthew Szekalski <mattszekalski@icloud.com>
2018-01-07 19:20:32 -0500

    a different way of doing live chat

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import GreetingContainer from './greeting/greeting_container.js';
 import SessionFormContainer from './session_form/session_form_container.js';
+import ChatFormContainer from './chat/chat_form_container.js';
 import { Route, HashRouter, Link} from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from "../util/route_util.jsx";
 import WelcomeContainer from "./welcome";
@@ -13,6 +14,7 @@ const App = () => (
     <AuthRoute path="/login" component={SessionFormContainer} />
     <AuthRoute path="/signup" component={SessionFormContainer} />
     <AuthRoute path="/" exact component={SplashPage} />
+    // <ProtectedRoute path="/home" exact component={ChatFormContainer} />
   </div>
 );
 
---
+++ frontend/components/chat/chat_form.jsx
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+class ChatForm extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+    currentChatMessage: '',
+    chatLogs: []
+  };
+}
+
+handleSendEvent(event) {
+  event.preventDefault();
+  this.chats.create(this.state.currentChatMessage);
+  this.setState({
+    currentChatMessage: ''
+  });
+}
+
+handleChatInputKeyPress(event) {
+  if(event.key === 'Enter') {
+    this.handleSendEvent(event);
+  }
+}
+
+updateCurrentChatMessage(event) {
+  this.setState({
+    currentChatMessage: event.target.value
+  });
+}
+
+createSocket() {
+  let cable = Cable.createConsumer('ws://localhost:3000/cable');
+  this.chats = cable.subscriptions.create({
+    channel: 'ChatChannel'
+  }, {
+    connected: () => {},
+    received: (data) => {
+      let chatLogs = this.state.chatLogs;
+      chatLogs.push(data);
+      this.setState({ chatLogs: chatLogs });
+    },
+    create: function(chatContent) {
+      this.perform('create', {
+        content: chatContent
+      });
+    }
+  });
+}
+
+renderChatLog() {
+  return this.state.chatLogs.map((el) => {
+    return (
+      <li key={`chat_${el.id}`}>
+        <span className='chat-message'>{ el.content }</span>
+        <span className='chat-created-at'>{ el.created_at }</span>
+      </li>
+    );
+  });
+}
+
+componentWillMount() {
+  this.createSocket();
+}
+
+  render() {
+    return (
+      <div className='ChatForm'>
+        <div className='stage'>
+          <h1>Chat</h1>
+            <ul className='chat-logs'>
+              { this.renderChatLog() }
+            </ul>
+            <input
+              onKeyPress={ (e) => this.handleChatInputKeyPress(e) }
+              value={ this.state.currentChatMessage }
+              onChange={ (e) => this.updateCurrentChatMessage(e) }
+              type='text'
+              placeholder='Enter your message...'
+              className='chat-input' />
+            <button
+              onClick={ (e) => this.handleSendEvent(e) }
+              className='send'>
+              Send
+            </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(ChatForm);
