84e3a3f52a479b13fb573b707b9f1e5ef1de02d5
Matthew Szekalski <mattszekalski@icloud.com>
2018-01-10 21:01:57 -0500

    fixed migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+Channel.destroy_all
+ChatMessage.destroy_all
+
 u1 = User.create(username: "demo-user", password: "password")
 
 c1 = Channel.create(topic: "App Academy")
