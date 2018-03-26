daeaa4b03ea918d1cf13ac2c2739d2a266029e25
Matthew Szekalski <mattszekalski@icloud.com>
2018-03-26 16:52:22 -0400

    more styling

--- frontend/components/channel/channel_form.jsx
+++ frontend/components/channel/channel_form.jsx
@@ -1,92 +1,76 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router';
-import { Route, Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { withRouter } from "react-router";
+import { Route, Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 class ChannelForm extends React.Component {
   constructor(props) {
-   super(props);
-   this.handleSubmit = this.handleSubmit.bind(this);
-   let newChannel = { topic: '' };
-  this.state = newChannel;
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    let newChannel = { topic: "" };
+    this.state = newChannel;
+  }
 
- }
+  update(field) {
+    return e => {
+      this.setState({ [field]: e.target.value });
+    };
+  }
 
- update(field) {
-   return e => {
-     this.setState({[field]: e.target.value});
-   };
- }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props
+      .createChannel(this.state)
+      .then(() => this.props.history.push("/home/"));
+  }
 
- handleSubmit(e) {
-   
-   e.preventDefault();
-   this.props.createChannel(this.state).then(() => this.props.history.push('/home'));
- }
+  hide() {
+    document.getElementById("channel-form-container").classList.add("hidden");
+  }
 
- hide() {
-   document.getElementById("channel-form-container").classList.add('hidden');
- }
-
-  render () {
-
+  render() {
     return (
-      <div id='channel-form-container' className='hidden'>
-        <div className='channel-form'>
-          <div className='new-channel-title-container'>
-
-            <h1 className='new-channel-title'>Create a channel</h1>
+      <div id="channel-form-container" className="hidden">
+        <div className="channel-form">
+          <div className="new-channel-title-container">
+            <h1 className="new-channel-title">Create a channel</h1>
           </div>
-          <div className='channel-description-container'>
-            <p1 className='new-channel-description'>
-              Channels are where your members communicate. They&apos;re best when organized around a topic
-              -- #leads, for example.
+          <div className="channel-description-container">
+            <p1 className="new-channel-description">
+              Channels are where your members communicate. They&apos;re best
+              when organized around a topic -- #leads, for example.
             </p1>
           </div>
-        <form
-          onSubmit={this.handleSubmit}
-          >
-
-
-          <input
-            className="channel-name-input"
-            type='text'
-            value={this.state.topic}
-            onChange={this.update('topic')}
-            placeholder='e.g leads'
+          <form onSubmit={this.handleSubmit}>
+            <input
+              className="channel-name-input"
+              type="text"
+              value={this.state.topic}
+              onChange={this.update("topic")}
+              placeholder="e.g leads"
             />
-          <footer className='channel-form-footer'>
-
-
-            <button
-              className="channel-button"
-              type="submit"
-              value='text'
+            <footer className="channel-form-footer">
+              <button
+                className="channel-button"
+                type="submit"
+                value="text"
+                onClick={this.hide}
               >
-              Create Channel
-            </button>
+                Create Channel
+              </button>
 
               <input
                 className="cancel-button"
                 type="button"
                 value="Cancel"
                 onClick={this.hide}
-                />
-
-
-
-
-          </footer>
-        </form>
-
-
-      </div>
-
+              />
+            </footer>
+          </form>
+        </div>
       </div>
     );
   }
-
 }
-
 
 export default withRouter(ChannelForm);
