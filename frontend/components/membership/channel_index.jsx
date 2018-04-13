4f51958bb34399b4af6d59d282f7741aa536b0b3
Matthew Szekalski <mattszekalski@icloud.com>
2018-04-13 15:51:38 -0400

    search index only renders joined/not joined channels

--- frontend/components/membership/channel_index.jsx
+++ frontend/components/membership/channel_index.jsx
@@ -6,52 +6,88 @@ class ChannelIndex extends React.Component {
     super(props);
   }
 
+  joinedChannels() {
+    for (let i = 0; i < this.props.channels.length; i++) {
+      if (
+        this.props.channels[i].member_ids.includes(this.props.user.id) &&
+        this.props.channels[i].topic
+          .toLowerCase()
+          .includes(this.props.search.toLowerCase())
+      ) {
+        return (
+          <div className="bottom-membership-search-div">
+            <div className="channel-index-list-header">
+              Channels you belong to
+            </div>
+            {this.props.channels.map(channel => {
+              if (
+                channel.member_ids.includes(this.props.user.id) &&
+                channel.topic
+                  .toLowerCase()
+                  .includes(this.props.search.toLowerCase())
+              ) {
+                return (
+                  <ChannelIndexItem
+                    channel={channel}
+                    key={channel.id}
+                    user={this.props.user}
+                    receiveChannel={this.props.receiveChannel}
+                    hide={this.props.hide}
+                    history={this.props.history}
+                  />
+                );
+              }
+            })}
+          </div>
+        );
+      }
+    }
+  }
+
+  notJoinedChannels() {
+    for (let i = 0; i < this.props.channels.length; i++) {
+      if (
+        !this.props.channels[i].member_ids.includes(this.props.user.id) &&
+        this.props.channels[i].topic
+          .toLowerCase()
+          .includes(this.props.search.toLowerCase())
+      ) {
+        return (
+          <div className="top-membership-search-div">
+            <div className="channel-index-list-header">
+              Channels you can join
+            </div>
+            {this.props.channels.map(channel => {
+              if (
+                !channel.member_ids.includes(this.props.user.id) &&
+                channel.topic
+                  .toLowerCase()
+                  .includes(this.props.search.toLowerCase())
+              ) {
+                return (
+                  <ChannelIndexItem
+                    channel={channel}
+                    key={channel.id}
+                    user={this.props.user}
+                    createMembership={this.props.createMembership}
+                    hide={this.props.hide}
+                    history={this.props.history}
+                  />
+                );
+              }
+            })}
+          </div>
+        );
+      }
+    }
+  }
+
   render() {
     return (
       <div className="channel-index-list-div">
         <div className="channel-index-list">
-          <div className="channel-index-list-header">Channels you can join</div>
-          {this.props.channels.map(channel => {
-            if (
-              !channel.member_ids.includes(this.props.user.id) &&
-              channel.topic
-                .toLowerCase()
-                .includes(this.props.search.toLowerCase())
-            ) {
-              return (
-                <ChannelIndexItem
-                  channel={channel}
-                  key={channel.id}
-                  user={this.props.user}
-                  createMembership={this.props.createMembership}
-                  hide={this.props.hide}
-                  history={this.props.history}
-                />
-              );
-            }
-          })}
-          <div className="channel-index-list-header">
-            Channels you belong to
-          </div>
-          {this.props.channels.map(channel => {
-            if (
-              channel.member_ids.includes(this.props.user.id) &&
-              channel.topic
-                .toLowerCase()
-                .includes(this.props.search.toLowerCase())
-            ) {
-              return (
-                <ChannelIndexItem
-                  channel={channel}
-                  key={channel.id}
-                  user={this.props.user}
-                  hide={this.props.hide}
-                  history={this.props.history}
-                  receiveChannel={this.props.receiveChannel}
-                />
-              );
-            }
-          })}
+          {this.notJoinedChannels()}
+          {this.joinedChannels()}
         </div>
       </div>
     );
