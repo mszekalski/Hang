637af5d99078a4bff1f1395daddb606fde4af502
Matthew Szekalski <mattszekalski@icloud.com>
2018-05-04 19:11:35 -0400

    started coding up thread form

---
+++ frontend/components/thread/thread_form.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router";
+import { Route, Link } from "react-router-dom";
+import { connect } from "react-redux";
+
+class ThreadForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.hide = this.hide.bind(this);
+    let newThread = {
+      creator_id: this.props.currentUser.id,
+      membershipable_type: "Thread"
+    };
+    this.state = newThread;
+    this.update = this.update.bind(this);
+  }
+}
---
+++ frontend/components/thread/thread_form_container.js
@@ -0,0 +1,26 @@
+import { connect } from "react-redux";
+import {
+  createThread,
+  fetchThread,
+  clearErrors
+} from "../../actions/thread_actions.js";
+import { fetchAllUsers } from "../../actions/user_actions.js";
+import ThreadForm from "./thread_form";
+
+const mapStateToProps = state => {
+  return {
+    errors: state.errors.thread,
+    currentUser: state.session.currentUser
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    createThread: thread => dispatch(createThread(thread)),
+    fetchThread: thread => dispatch(fetchThread(thread)),
+    fetchAllUsers: () => dispatch(fetchAllUsers()),
+    clearErrors: () => dispatch(clearErrors())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ThreadForm);
