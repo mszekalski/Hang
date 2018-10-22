bd7d2a33f3855d34ff9ac134245c4e3964276cd5
Matthew Szekalski <mattszekalski@icloud.com>
2018-10-22 17:44:42 -0400

    more seed fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@ User.destroy_all
 Channel.destroy_all
 ChatMessage.destroy_all
 DirectThread.destroy_all
+Membership.destroy_all
 
 u1 = User.create(username: "demo-user", password: "password")
 u2 = User.create(username: "demo-user2", password: "password")
@@ -27,8 +28,9 @@ m1 = ChatMessage.create(content: "This is great!", user_id: u2.id, chatable_id: 
 m1 = ChatMessage.create(content: "Thanks for the help!", user_id: u1.id, chatable_id: d1.id, chatable_type: "DirectThread")
 
 membership1 = Membership.create(user_id: u1.id, membershipable_id: c1.id, membershipable_type: "Channel")
-membership1 = Membership.create(user_id: u2.id, membershipable_id: c1.id, membershipable_type: "Channel")
-membership1 = Membership.create(user_id: u3.id, membershipable_id: c1.id, membershipable_type: "Channel")
+membership2 = Membership.create(user_id: u2.id, membershipable_id: c1.id, membershipable_type: "Channel")
+membership3 = Membership.create(user_id: u3.id, membershipable_id: c1.id, membershipable_type: "Channel")
 
-membership1 = Membership.create(user_id: u2.id, membershipable_id: d1.id, membershipable_type: "DirectThread")
-membership1 = Membership.create(user_id: u3.id, membershipable_id: d1.id, membershipable_type: "DirectThread")
+membership14 = Membership.create(user_id: u2.id, membershipable_id: d1.id, membershipable_type: "DirectThread")
+membership15 = Membership.create(user_id: u3.id, membershipable_id: d1.id, membershipable_type: "DirectThread")
+membership16 = Membership.create(user_id: u1.id, membershipable_id: d1.id, membershipable_type: "DirectThread")
